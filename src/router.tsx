ca1aadbd698197f4cc4b51a7bf985e25a6211397
appsbysam <mrsambo@gmail.com>
2026-08-26 02:15:15 +0300

    Fix router base path for GitHub Pages

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: "/adcdruze-connect",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
